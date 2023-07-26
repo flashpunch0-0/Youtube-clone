dbf388cb7a83371cf87ae4394a44457a8c7afee6
Kratik <kratikbohra5@gmail.com>
2023-07-27 03:14:49 +0530

    Working CategoriesBar , click on categories to load videos related to it

--- src/Components/Categoriesbar/Categoriesbar.jsx
+++ src/Components/Categoriesbar/Categoriesbar.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { getVideosByCategory } from '../../redux/action/videos.action'
 const keywords = ["kittu",
     "dsa",
     "cricket",
@@ -9,23 +11,31 @@ const keywords = ["kittu",
     "Chennai super",
     "Mumbai",
     "mumbai city fc",
-    "Arsenal",
-    "Chennai super",
-    "Mumbai",
-    "Arsenal",
-    "Chennai super",
-    "Mumbai",
-    "mumbai city fc",
-    "Arsenal",
-    "Chennai super",
-    "Mumbai",
+    "real madrid",
+    "barcelon",
+    "thiery henry",
+    "martin odegaard",
+    "bergkamp",
+    "arsene wenger",
+    "saka",
+    "martinelli",
+    "declan rice",
+    "mikel arteta",
 ]
 
 const Categoriesbar = () => {
+    const [activeElement, setactiveElement] = useState("All")
+    const dispatch = useDispatch()
+    const handleClick = value => {
+        setactiveElement(value)
+        dispatch(getVideosByCategory(value))
+        // let appliedClass = activeElement === value ? "active mr-4 p-2 whitespace-nowrap  rounded-lg  hover:text-black bg-bordercolor hover:bg-white " : " mr-4 p-2 whitespace-nowrap  rounded-lg  hover:text-black bg-bordercolor hover:bg-white ";
+    }
 
     return (
-        <div className='Categoriesbar py-2 px-0  text-xs   overflow-x-scroll '>
-            {keywords.map((value, i) => <span className=' mr-4 p-2 whitespace-nowrap  rounded-lg  hover:text-black bg-bordercolor hover:bg-white ' key={i}>{value}</span>)}
+        <div className='Categoriesbar py-2 px-0  text-xs   overflow-x-scroll ' >
+            {keywords.map((value, i) => <span onClick={() => handleClick(value)} className='  mr-4 p-2 whitespace-nowrap  rounded-lg  hover:text-black bg-bordercolor hover:bg-white ' key={i}>{value}</span>)}
+            {/* {keywords.map((value, i) => <span onClick={() => handleClick(value)} className={appliedClass} key={i}>{value}</span>)} */}
         </div>
     )
 }
--- src/redux/action/videos.action.js
+++ src/redux/action/videos.action.js
@@ -5,6 +5,7 @@ import {
 } from "../actionType";
 
 import request from "../../api";
+import { useDispatch } from "react-redux";
 
 export const getPopularVideos = () => async (dispatch) => {
   try {
@@ -37,3 +38,69 @@ export const getPopularVideos = () => async (dispatch) => {
     });
   }
 };
+
+// export const getVideosByCategory = (keyword) => async (dispatch, getState) => {
+//   try {
+//     dispatch({
+//       type: HOME_VIDEOS_REQUEST,
+//     });
+//     const { data } = await request("/search", {
+//       params: {
+//         part: "snippet",
+
+//         maxResults: 20,
+//         pageToken: getState().homeVideos.nextPageToken,
+//         q: keyword,
+//         type: "video",
+//       },
+//     });
+
+//     dispatch({
+//       type: HOME_VIDEOS_SUCCESS,
+//       payload: {
+//         videos: data.items,
+//         nextPageToken: data.nextPageToken,
+//         category: keyword,
+//       },
+//     });
+//   } catch (error) {
+//     console.log(error.message);
+//     dispatch({
+//       type: HOME_VIDEOS_FAIL,
+//       payload: error.message,
+//     });
+//   }
+// };
+
+export const getVideosByCategory =
+  (keyword) => async (dispatch, getState) => {
+    try {
+      dispatch({
+        type: HOME_VIDEOS_REQUEST
+      });
+      const { data } = await request("/search", {
+        params: {
+          part: "snippet",
+          maxResults: 20,
+          pageToken: getState().homeVideos.nextPageToken,
+          q: keyword,
+          type: "video"
+        },
+      });
+
+      dispatch({
+        type: HOME_VIDEOS_SUCCESS,
+        payload: {
+          videos: data.items,
+          nextPageToken: data.nextPageToken,
+          category: keyword,
+        },
+      });
+    } catch (error) {
+      console.log(error.message)
+      dispatch({
+        type: HOME_VIDEOS_FAIL,
+        payload: error.message,
+      });
+    }
+  };
